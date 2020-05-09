893dd1fbf09670595d44f48d55037ebb5f189a01
Giancarlo Horta <giancarlohorta@gmail.com>
2020-05-09 17:11:32 -0300

    Refatoração Accordion

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import initAnimationScroll from "./modules/scroll-animacao.js";
+import Accordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
-import initAccordion from "./modules/accordion.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
@@ -9,14 +8,17 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import initAnimationScroll from "./modules/scroll-animacao.js";
 
 const scrollSuave = new ScrollSuave('[data-anime="suave"] a[href^="#"]');
 scrollSuave.init();
 
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 initMenuMobile();
 initAnimationScroll();
 initTabNav();
-initAccordion();
 initModal();
 initTooltip();
 initDropdownMenu();
